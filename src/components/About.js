7e0417a1647123cffaa84e6b3cbcecd0fbde2a2d
Tyler Collier <git@tylercollier.com>
2017-04-12 02:31:30 -0700

    Show dynamic loading of component

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,37 @@
 import React from 'react';
 
-const About = () => <div>About</div>;
+// const About = () => <div>About</div>;
+
+class About extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      MyComponent: undefined,
+    }
+  }
+
+  componentWillMount() {
+    setTimeout(() => {
+      System.import('./MyComponent').then(module => {
+        setTimeout(() => {
+          this.setState({ MyComponent: module.default })
+        }, 1000)
+      })
+    }, 500)
+  }
+
+  render() {
+    const { MyComponent } = this.state
+    if (MyComponent) {
+      return (
+        <div>
+          <h1>About</h1>
+          <MyComponent />
+        </div>
+      )
+    }
+    return <div>Loading MyComponent</div>
+  }
+}
 
 export default About;
---
+++ src/components/MyComponent.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const MyComponent = () => <div>This is MyComponent!</div>;
+
+export default MyComponent;
